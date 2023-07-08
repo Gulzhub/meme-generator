f17f704924689649db7e9d2469d29fc8de374684
Gulzhub <gulzarshariff341@gmail.com>
2023-07-08 15:49:31 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 export default function Header() {
   return (
     <header className="header">
-      <img src="./images/troll-face.png" className="header--image" />
+      <img
+        src="./images/troll-face.png"
+        className="header--image"
+        alt="smiling meme"
+      />
       <h2 className="header--title">Meme Generator</h2>
     </header>
   );
