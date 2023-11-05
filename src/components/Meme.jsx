20cf7eee034e45cbffa875a8b3daeaa1c3775949
Gulzhub <gulzarshariff341@gmail.com>
2023-11-05 11:30:14 +0530

    added download button

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -34,6 +34,63 @@ export default function Meme() {
     }));
   }
 
+  <button className="form--button" onClick={downloadMeme}>
+    Save Meme 📥
+  </button>;
+
+  function downloadMeme() {
+    const img = document.querySelector(".meme--image");
+    // console.log(img.clientHeight);
+    // Create a canvas element
+    const canvas = document.createElement("canvas");
+    canvas.width = img.clientWidth; // Adjust the canvas size as needed
+    canvas.height = img.clientHeight; // Adjust the canvas size as needed
+
+    // Get the 2D drawing context
+    const ctx = canvas.getContext("2d");
+
+    // Load the meme image
+    const image = new Image();
+    image.crossOrigin = "Anonymous"; //to avoid the "tainted canvas" error
+    image.src = meme.randomImage;
+    image.onload = () => {
+      // Draw the meme image on the canvas
+      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+      // Set font and text properties for top text
+      ctx.font = "bold 32px impact, sans-serif";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+
+      // Apply text shadow
+      ctx.shadowColor = "#000";
+      ctx.shadowBlur = 10; // Adjust the shadow blur as needed
+      ctx.lineWidth = 2;
+      ctx.letterSpacing = "1px";
+
+      // Draw top text on the canvas
+      const topText = meme.topText.toUpperCase();
+      ctx.strokeText(topText, canvas.width / 2, 40);
+      ctx.fillText(topText, canvas.width / 2, 40);
+
+      // Set font and text properties for bottom text
+      ctx.font = "bold 32px impact, sans-serif";
+      ctx.textAlign = "center";
+
+      // Draw bottom text on the canvas
+      const bottomText = meme.bottomText.toUpperCase();
+      ctx.strokeText(bottomText, canvas.width / 2, canvas.height - 20);
+      ctx.fillText(bottomText, canvas.width / 2, canvas.height - 20);
+
+      // Create a download link
+      const dataURL = canvas.toDataURL("image/jpeg");
+      const a = document.createElement("a");
+      a.href = dataURL;
+      a.download = "gulz-meme.jpg";
+      a.click();
+    };
+  }
+
   return (
     <main>
       <div className="form">
@@ -57,10 +114,15 @@ export default function Meme() {
           Get a new meme image 🖼
         </button>
       </div>
-      <div className="meme">
-        <img src={meme.randomImage} className="meme--image" />
-        <h2 className="meme--text top">{meme.topText}</h2>
-        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      <div className="wrap">
+        <div className="meme">
+          <img src={meme.randomImage} className="meme--image" />
+          <h2 className="meme--text top">{meme.topText}</h2>
+          <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        </div>
+        <button className="dwnld--button" onClick={downloadMeme}>
+          Save meme
+        </button>
       </div>
     </main>
   );
